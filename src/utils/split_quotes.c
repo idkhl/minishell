28aaaddc286d0187619ecb695bb405ee2a699a46
idkhl <ines.dakhlaoui@gmail.com>
2024-08-24 19:51:30 +0200

    started split quotes

--- src/utils/split_quotes.c
+++ src/utils/split_quotes.c
@@ -6,7 +6,7 @@
 /*   By: idakhlao <idakhlao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 15:20:03 by idakhlao          #+#    #+#             */
-/*   Updated: 2024/08/23 19:31:53 by idakhlao         ###   ########.fr       */
+/*   Updated: 2024/08/24 19:49:02 by idakhlao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,65 @@ int	count_words(char *line, char c)
 	return (size);
 }
 
+char	*malloc_quote(char *line, char quote, int i)
+{
+	char	*word;
+	int		start;
+	int		len;
+	int		j;
+
+	start = i;
+	len = 0;
+	while (line[i] != quote && line[i])
+	{
+		len++;
+		i++;
+	}
+	word = (char *)malloc((len + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (j < len)
+	{
+		word[j] = line[start + j];
+		j++;
+	}
+	word[len] = '\0';
+	return (word);
+}
+
+char	*malloc_word(char *line, char c, int i)
+{
+	char	*word;
+	int		start;
+	int		len;
+	int		j;
+
+	start = i;
+	len = 0;
+	j = 0;
+	while (line[i] && line[i] != c)
+	{
+		len++;
+		i++;
+	}
+	word = (char *)malloc((len + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (j < len)
+	{
+		word[j] = line[start + j];
+		j++;
+	}
+	word[len] = '\0';
+	return (word);
+}
+
 char	**insert_words(char **word, char *line, char c)
 {
-	int	i;
-	int	j;
-	int	in_quote;
+	int		i;
+	int		j;
+	int		in_quote;
+	char	quote;
 
 	i = 0;
 	j = 0;
@@ -61,8 +115,15 @@ char	**insert_words(char **word, char *line, char c)
 		if (line[i] == 34 || line[i] == 39)
 		{
 			in_quote = 1;
+			quote = line[i];
+			i++;
 		}
+		word[j] = malloc_quote(line, quote, i);
+		printf("test\n");
+		i++;
 	}
+	printf("[%s]\n", word[j]);
+	return (word);
 }
 
 char	**split_quotes(char *line, char c)
@@ -75,5 +136,5 @@ char	**split_quotes(char *line, char c)
 	if (!split)
 		return (NULL);
 	split = insert_words(split, line, c);
-	return (ft_split(line, c));
+	return (split);
 }
