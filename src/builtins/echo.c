5fbe6daa1d05cece200f6ca47b4709b95ca7b699
idkhl <ines.dakhlaoui@gmail.com>
2024-10-29 11:49:00 +0100

    merge ines

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: idakhlao <idakhlao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 17:15:27 by idakhlao          #+#    #+#             */
-/*   Updated: 2024/10/29 11:17:41 by idakhlao         ###   ########.fr       */
+/*   Updated: 2024/10/29 11:46:15 by idakhlao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,42 @@ int	check_echo(char *str)
 	return (1);
 }
 
+// void	build_echo(char **tab)
+// {
+// 	int	i;
+// 	int	nl;
+
+// 	i = 0;
+// 	nl = 0;
+// 	if (ft_strcmp(tab[i], "echo") == 0)
+// 		i++;
+// 	while (tab[i] && ft_strncmp(tab[i], "-n", 2) == 0)
+// 	{
+// 		nl = check_echo(tab[i]);
+// 		i++;
+// 	}
+// 	if (tab[i])
+// 	{
+// 		while (tab[i])
+// 		{
+// 			printf("%s", tab[i]);
+// 			if (tab[i + 1])
+// 				printf(" ");
+// 			i++;
+// 		}
+// 	}
+// 	if (nl == 0)
+// 		printf("\n");
+// }
+
 void	build_echo(char **tab)
 {
 	int	i;
 	int	nl;
 
-	i = 0;
+	i = 1;
 	nl = 0;
-	if (ft_strcmp(tab[i], "echo") == 0)
-		i++;
-	while (tab[i] && ft_strncmp(tab[i], "-n", 2) == 0)
+	if (tab [i] && ft_strncmp(tab[i], "-n", 2) == 0)
 	{
 		nl = check_echo(tab[i]);
 		i++;
@@ -56,11 +82,3 @@ void	build_echo(char **tab)
 	if (nl == 0)
 		printf("\n");
 }
-
-// void	build_echo(char **tab)
-// {
-// 	int	i;
-// 	int	nl;
-
-// 	i = 0;
-// }
